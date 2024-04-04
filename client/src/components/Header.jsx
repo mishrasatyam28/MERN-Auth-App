07762d516511529eb89606b3ceeb5616017ff4d2
Satyam Mishra <satyamsumit8750@gmail.com>
2024-04-04 09:15:43 +0530

    complete sign up page functionality

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 export default function Header() {
   return (
-    <div className="bg-slate-200">
+    <div className="bg-slate-400">
       <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
         <Link to="/">
           <h1 className="font-bold">Auth App</h1>
@@ -14,8 +14,8 @@ export default function Header() {
           <Link to="/about">
             <li>About</li>
           </Link>
-          <Link to="/sign-up">
-            <li>SignUp</li>
+          <Link to="/sign-in">
+            <li>Sign In</li>
           </Link>
         </ul>
       </div>
